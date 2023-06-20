95650d4f7de64922ceb113ffd5aa98482d57fab4
Magusbear <70155173+Magusbear@users.noreply.github.com>
2023-06-20 21:16:46 +0200

    First submit
    First python scraper test, working.

---
+++ metal-archives-scraper.py
@@ -0,0 +1,52 @@
+import requests
+import time
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
+
+# Set up Firefox webdriver in headless mode
+options = Options()
+options.add_argument('-headless')
+driver = webdriver.Firefox(options=options)
+
+# For local testing
+# filename = "test.html"
+# with open(filename, "r") as file:
+#     html_content = file.read()
+# metalSoup = BeautifulSoup(html_content, "html.parser")
+# for local Testing
+
+# Need selenium to delay the scraping because the site needs ~5 seconds to load
+driver.get("https://www.metal-archives.com/release/upcoming")
+time.sleep(8)
+html = driver.page_source
+
+# For actual scraping
+# url = "https://www.metal-archives.com/release/upcoming"
+# response = requests.get(url)
+# metalSoup = BeautifulSoup(response.content, "html.parser")
+metalSoup = BeautifulSoup(html, "html.parser")
+# For actual scraping
+
+driver.quit()
+
+rows = metalSoup.find_all("tr")
+
+data_list = []
+
+for row in rows:
+    cells = row.find_all("td")
+    
+    if len(cells) >= 6:
+        band = cells[0].find("a").get_text()
+        album = cells[1].find("a").get_text()
+        release_type = cells[2].get_text()
+        genre = cells[3].get_text()
+        release_date = cells[4].get_text()
+        added_date = cells[5].get_text()
+        data_list.append([band, album, release_type, genre, release_date, added_date])
+
+for data in data_list:
+    genre = data[3]
+    if "deathcore" in genre.lower():
+        print(data)
