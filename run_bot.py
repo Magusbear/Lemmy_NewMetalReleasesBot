987af43be0d842c8839dd0a6309f118d5d7e2fa4
Magusbear <mndt@posteo.net>
2023-06-22 12:49:33 +0200

    Added toggle for live/local mode
    With "set_for_production" set to false (the default for now) the bot will only post its data into the console instead of posting it to Lemmy.
    Useful for testing the scrapers.

--- run_bot.py
+++ run_bot.py
@@ -1,7 +1,7 @@
 import metal_archives_scraper.ma_scraper as ma_scraper
 import Utils.Bot_Utils as Bot_Utils
 import lemmy_api.lemmy_api_tasker as lemmy_api_tasker
-from config.config import wantedGenre, lemmy_instance, title, community_id, last_post_id, release_day, username_or_email, password, last_post_date
+from config.config import set_for_production, wantedGenre, lemmy_instance, title, community_id, last_post_id, release_day, username_or_email, password, last_post_date
 from dotenv import dotenv_values, set_key
 import time
 
@@ -27,15 +27,12 @@ if input("Do you want to change settings? 'y/n'") == "y":
         set_key(".env", key, value)
 
 # Bot
-does_post_exist = False
-loop_bot = True
-# Bot runs only on friday
-while loop_bot == True:
+# Post to Lemmy function
+def post_to_lemmy():
     if currentWeekday == release_day and does_post_exist == False:
         # Call scraper to get release list
         wanted_list,wantedMonthString = ma_scraper.get_releaseList(wantedGenre,wantedDay,wantedMonth)
-
-        # Print data into the console (for now)
+        # Create body text
         body = ""
         body += (f'{wantedGenre} releases for {wantedDay} of {wantedMonthString}: \n')
         for type in release_type_list:                                  # cheeky way of sorting the list
@@ -44,8 +41,9 @@ while loop_bot == True:
                 body += string
             
 
-
+        # Get User Token
         user_token = lemmy_api_tasker.get_user_token(username_or_email, password, lemmy_instance)
+        # Post through Lemmy Api
         post_successful, post_id, posted_date = lemmy_api_tasker.create_post(user_token, int(community_id), lemmy_instance, title, body)
         
         # For testing
@@ -61,11 +59,6 @@ while loop_bot == True:
             for key, value in env_vars.items():
                 set_key(".env", key, value)
         does_post_exist = True
-
-
-
-
-
     else:
         print("Waiting 7 days to post again...")
         # time.sleep(10)
@@ -74,3 +67,47 @@ while loop_bot == True:
         # wantedMonth = "June"    #Test
         time.sleep(43200)
         today, currentWeekday, wantedMonth, wantedDay = Bot_Utils.get_date()    # refresh date
+
+# Scrapes and creates title and body but posts it only into the console, not to Lemmy
+def post_locally():
+    if currentWeekday == release_day and does_post_exist == False:
+        # Call scraper to get release list
+        wanted_list,wantedMonthString = ma_scraper.get_releaseList(wantedGenre,wantedDay,wantedMonth)
+        # Create body text
+        body = ""
+        body += (f'{wantedGenre} releases for {wantedDay} of {wantedMonthString}: \n')
+        for type in release_type_list:                                  # cheeky way of sorting the list
+            releases_list = Bot_Utils.sortByReleaseType(type, wanted_list,wantedGenre,wantedDay,wantedMonthString)
+            for string in releases_list:
+                body += string
+
+        # For testing
+        print(title)
+        print(body)
+        post_successful = True 
+        post_id = "1"
+        posted_date = "15.03.2023"
+
+        if post_successful == True:
+            env_vars['last_post_id'] = str(post_id)
+            env_vars['last_post_date'] = str(posted_date)
+            for key, value in env_vars.items():
+                set_key(".env", key, value)
+        does_post_exist = True
+    else:
+        print("Waiting 7 days to post again...")
+        # time.sleep(10)
+        # currentWeekday = "4"      #Test
+        # wantedDay = "23"          #Test
+        # wantedMonth = "June"    #Test
+        time.sleep(43200)
+        today, currentWeekday, wantedMonth, wantedDay = Bot_Utils.get_date()    # refresh date        
+
+does_post_exist = False
+loop_bot = True
+# Bot runs only on friday
+while loop_bot == True:
+    if set_for_production == True:
+        post_to_lemmy()
+    else:
+        post_locally()                          # Scrapes and creates title and body but posts it only into the console, not to Lemmy
